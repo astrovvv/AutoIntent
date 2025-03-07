e1753f5d6d2d4d702b5d1fe5bd112eff3e2c522e
Алексеев Илья <44509110+voorhs@users.noreply.github.com>
2025-03-07 14:01:58 +0300

    Docs/automl tutorials (#163)
    * update pipeline tutorials
    
    * minor fix
    
    * update tutorials

--- user_guides/basic_usage/03_automl.py
+++ user_guides/basic_usage/03_automl.py
@@ -76,6 +76,42 @@ custom_pipeline.set_config(logging_config)
 
 # %% [markdown]
 """
+## Default Transformers
+
+One can specify what embedding model and cross-encoder model want to use along with default settings:
+"""
+
+# %%
+from autointent.configs import EmbedderConfig, CrossEncoderConfig
+
+custom_pipeline.set_config(EmbedderConfig(model_name="prajjwal1/bert-tiny", device="cpu"))
+custom_pipeline.set_config(CrossEncoderConfig(model_name="cross-encoder/ms-marco-MiniLM-L2-v2", max_length=8))
+
+# %% [markdown]
+"""
+See the docs for %mddoclink(class,configs,EmbedderConfig) and %mddoclink(class,configs,CrossEncoderConfig) for options available to customize.
+"""
+
+# %% [markdown]
+"""
+## Cross-Validation vs Hold-Out Validation
+
+If you have lots of training and evaluation data, you can use default hold-out validation strategy. If not, you can choose cross-validation and spend a little more time but utilize the full amount of available data for better hyperparameter tuning.
+
+This behavior is controlled with %mddoclink(class,configs,DataConfig):
+"""
+
+# %%
+from autointent.configs import DataConfig
+custom_pipeline.set_config(DataConfig(scheme="cv", n_folds=3))
+
+# %% [markdown]
+"""
+See the docs for %mddoclink(class,configs,DataConfig) for other options available to customize.
+"""
+
+# %% [markdown]
+"""
 ## Complete Example
 """
 
@@ -99,7 +135,43 @@ logging_config = LoggingConfig()
 custom_pipeline.set_config(logging_config)
 
 # start auto-configuration
-custom_pipeline.fit(dataset)
+context = custom_pipeline.fit(dataset)
 
-# inference
+# inference on-the-fly
 custom_pipeline.predict(["hello world!"])
+
+# %% [markdown]
+"""
+## Dump Results
+
+One can save all results of auto-configuration process to file system (to ``LoggingConfig.dirpath``):
+"""
+
+# %%
+context.dump()
+
+# %% [markdown]
+"""
+Or one can dump only the configured pipeline to any desired location (by default ``LoggingConfig.dirpath``):
+"""
+
+# %%
+custom_pipeline.dump()
+
+# %% [markdown]
+"""
+## Load Pipeline for Inference
+"""
+
+# %%
+loaded_pipe = Pipeline.load(logging_config.dirpath)
+
+# %% [markdown]
+"""
+Since this notebook is launched automatically while building the docs, we will clean the space if you don't mind :)
+"""
+
+# %%
+import shutil
+
+shutil.rmtree(logging_config.dirpath)
